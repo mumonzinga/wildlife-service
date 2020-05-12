5bb0d30a87ad77482c9ab20f3e28a8cbfbe309f2
Mumo <mnzinga888@gmail.com>
2020-05-13 01:48:31 +0300

    add public static

---
+++ src/main/resources/public/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+// Animations init
+new WOW().init();
+
+var typed=new Typed(`#type`,{
+     strings:[" he reducing roars of the wild is of testimony of the inhumane brutality of us humans. I now know why it's better being wild than uncivilised” "],
+     backSpeed:70,
+     typeSpeed:80,
+     smartBackspace:true,
+     loop: true,
+   })
